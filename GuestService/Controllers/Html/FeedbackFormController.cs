63ffe3e88a38f10ee7db1f0abeded30ac308832f
PuntusEugene <kapral.20.it@gmail.com>
2016-04-18 15:07:07 +0300

    feedback form (Improvement #390)

---
+++ GuestService/Controllers/Html/FeedbackFormController.cs
@@ -0,0 +1,44 @@
+﻿namespace GuestService.Controllers.Html
+{
+    using System.Web.Mvc;
+    using GuestService.Models;
+    using Sm.System.Mvc.Language;
+    using System;
+    using Notifications;
+
+    public class FeedbackFormController : BaseController
+    {
+        [AllowAnonymous, ActionName("index")]
+        public ActionResult Index()
+        {
+            FeedbackFormContext model = new FeedbackFormContext();
+            
+            return base.View(model);
+        }
+
+        [AllowAnonymous, ActionName("Index"), HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Index(FeedbackFormContext model)
+        {
+            if (base.ModelState.IsValid)
+            {
+                try
+                {
+                    new SimpleEmailService().SendEmail<FeedbackFormContext>(Settings.EmailForCancellation,
+                                                        "send_feedback",
+                                                        "en",
+                                                        model);
+
+                    ((dynamic)base.ViewBag).success = true;
+                }
+                catch (Exception exception)
+                {
+                    base.ModelState.AddModelError("", exception);
+                    ((dynamic)base.ViewBag).success = false;
+                }
+            }
+
+            
+            return base.View(model);
+        }
+    }
+}
